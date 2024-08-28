e0b4c0a3b26023dee42eeb61d512bcbf2cd2e351
Javaria Ghafoor <jav@Javarias-MacBook-Pro.local>
2024-08-27 23:20:56 -0400

    Lab 1

---
+++ L1/src/User.java
@@ -0,0 +1,19 @@
+
+public class User {
+    private String name;
+    private Integer age;
+
+    public User(String name, @Nullable Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Nullable
+    public Integer getAge() {
+        return age;
+    }
+}
