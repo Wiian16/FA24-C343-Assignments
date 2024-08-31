ae49c58929d20fe2ad091072729048cc5755a813
Amr Sabry <sabry@indiana.edu>
2024-08-31 07:12:33 -0400

    A1

---
+++ A1/src/Main.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+/**
+ * Read Ch. 3 on Recursion.
+ * <p>
+ * Write recursive solutions for the following methods.
+ * <p>
+ * For each method, there is one test case provided in <code>MainTest.java</code>.
+ * Consult the test case to understand the expected behavior. Either before
+ * or after writing the recursive method, add more test cases of your own.
+ */
+
+public class Main {
+
+    /**
+     * Compute the set of all subsequences of a given string. A subsequence
+     * of a string is any string that can be obtained by deleting zero or more
+     * characters from the original string while maintaining the order of the
+     * remaining characters.
+     */
+    public static Set<String> allSubsequences(String str) {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Compute the minimum edit distance between two strings. The minimum edit
+     * distance is the minimum number of insertions, deletions, and replacements
+     * required to transform one string into another. Each insertion, deletion,
+     * or replacement has a cost of 1.
+     */
+    public static int minEditDistance(String s1, String s2) {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * Check if a given string is a subsequence of another.
+     */
+    public static boolean isSubsequence(String s1, String s2) {
+        // TODO
+        return false;
+    }
+
+    /**
+     * Compute the longest common subsequence of two strings.
+     */
+    public static String longestCommonSubsequence(String s1, String s2) {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Merge two strings in lexicographical order.
+     */
+    public static String mergeStrings(String s1, String s2) {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Sort the characters in a string using merge sort.
+     */
+    public static String mergeSort (String s) {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Compute the set of all permutations of a string.
+     */
+    public static Set<String> allPermutations(String str) {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Line wrapping. We are given a list of words, a maximum line width, and the
+     * number of spaces left on the current line. We want to wrap the words onto
+     * lines such that no line exceeds the maximum width. Words should be separated
+     * by a single space. If a word and its preceding space both fit on the current
+     * line, they are just added to the resulting string. Otherwise, the current
+     * word should be placed on the next line preceded by '\n'.
+     */
+    public static String lineWrap(List<String> words, int width, int space) {
+        // TODO
+        return null;
+    }
+}
