82360a31846dcc5b745af4989999077dde3c879b
Ian McGillivary <wiian16@icloud.com>
2024-09-09 16:48:22 -0400

    Finished with all methods but mergeSort

--- A1/src/Main.java
+++ A1/src/Main.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Read Ch. 3 on Recursion.
@@ -19,8 +22,20 @@ public class Main {
      * remaining characters.
      */
     public static Set<String> allSubsequences(String str) {
-        // TODO
-        return null;
+        Set<String> result = new HashSet<>();
+
+        if(str.isEmpty()) {
+            result.add(str);
+            return result;
+        }
+
+        Set<String> recResult = allSubsequences(str.substring(1));
+        for(String temp : recResult) {
+            result.add(str.charAt(0) + temp);
+            result.add(temp);
+        }
+
+        return result;
     }
 
     /**
@@ -30,48 +45,127 @@ public class Main {
      * or replacement has a cost of 1.
      */
     public static int minEditDistance(String s1, String s2) {
-        // TODO
-        return 0;
+        if(s1.equals(s2)) {
+            return 0;
+        }
+
+        if(s1.isEmpty()) {
+            return s2.length();
+        }
+
+        if(s2.isEmpty()) {
+            return s1.length();
+        }
+
+        if(s1.charAt(0) == s2.charAt(0)) {
+            return minEditDistance(s1.substring(1), s2.substring(1));
+        }
+
+        int replace = minEditDistance(s1.substring(1), s2.substring(1));
+        int insert = minEditDistance(s1, s2.substring(1));
+        int delete = minEditDistance(s1.substring(1), s2);
+
+        return 1 + Math.min(replace, Math.min(insert, delete));
     }
 
     /**
      * Check if a given string is a subsequence of another.
      */
     public static boolean isSubsequence(String s1, String s2) {
-        // TODO
-        return false;
+        if(s1.isEmpty()) {
+            return true;
+        }
+
+        int index = s2.indexOf(s1.charAt(0));
+
+        if(index == -1) {
+            return false;
+        }
+
+        return isSubsequence(s1.substring(1), s2.substring(index + 1));
     }
 
     /**
      * Compute the longest common subsequence of two strings.
      */
     public static String longestCommonSubsequence(String s1, String s2) {
-        // TODO
-        return null;
+        if(s1.isEmpty() || s2.isEmpty()) {
+            return "";
+        }
+
+        if(s1.charAt(0) == s2.charAt(0)) {
+            return s1.charAt(0) + longestCommonSubsequence(s1.substring(1), s2.substring(1));
+        }
+
+        String case1 = longestCommonSubsequence(s1.substring(1), s2);
+        String case2 = longestCommonSubsequence(s1, s2.substring(1));
+
+        if(case1.length() > case2.length()) {
+            return case1;
+        }
+
+        return case2;
     }
 
     /**
      * Merge two strings in lexicographical order.
      */
     public static String mergeStrings(String s1, String s2) {
-        // TODO
-        return null;
+        if(s1.isEmpty()) {
+            return s2;
+        }
+
+        if(s2.isEmpty()) {
+            return s1;
+        }
+
+        return s1.charAt(0) < s2.charAt(0) ? s1.charAt(0) + mergeStrings(s1.substring(1), s2) : s2.charAt(0) + mergeStrings(s1, s2.substring(1));
     }
 
     /**
      * Sort the characters in a string using merge sort.
      */
     public static String mergeSort (String s) {
-        // TODO
-        return null;
+        if(s.length() == 1) {
+            return s;
+        }
+
+        int split = s.length() / 2;
+
+        String left = mergeSort(s.substring(0, split));
+        String right = mergeSort(s.substring(split));
+
+        return mergeStrings(left, right);
     }
 
     /**
      * Compute the set of all permutations of a string.
      */
     public static Set<String> allPermutations(String str) {
-        // TODO
-        return null;
+        Set<String> result = new HashSet<>();
+
+        if(str.isEmpty()) {
+            result.add(str);
+            return result;
+        }
+
+        for(int i = 0; i < str.length(); i++) {
+            String next = swap(str, 0, i);
+
+            for(String temp : allPermutations(next.substring(1))) {
+                result.add(next.charAt(0) + temp);
+            }
+        }
+
+        return result;
+    }
+
+    private static String swap(String str, int a, int b) {
+        char[] charArray = str.toCharArray();
+        char temp = charArray[a];
+        charArray[a] = charArray[b];
+        charArray[b] = temp;
+        return String.valueOf(charArray);
     }
 
     /**
@@ -83,7 +177,18 @@ public class Main {
      * word should be placed on the next line preceded by '\n'.
      */
     public static String lineWrap(List<String> words, int width, int space) {
-        // TODO
-        return null;
+        ArrayList<String> wordsList = new ArrayList<>(words);
+
+        if(wordsList.isEmpty()) {
+            return "";
+        }
+
+        if(wordsList.getFirst().length() + 1 <= space) {
+            String temp = wordsList.removeFirst();
+            return " " + temp + lineWrap(wordsList, width, space - temp.length() - 1);
+        }
+
+        String temp = wordsList.removeFirst();
+        return "\n" + temp + lineWrap(wordsList, width, width - temp.length());
     }
 }
--- A1/test/MainTest.java
+++ A1/test/MainTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +20,37 @@ class MainTest {
         assertTrue(result.contains("bc"));
         assertTrue(result.contains("ac"));
         assertTrue(result.contains("abc"));
+
+        result = Main.allSubsequences("abcdefghijklmnop");
+
+        assertEquals(65536, result.size());
+
+        assertTrue(result.contains(""));
+        assertTrue(result.contains("a"));
+        assertTrue(result.contains("p"));
+        assertTrue(result.contains("ap"));
+        assertTrue(result.contains("abcdefghijklmnop"));
+/*
+        result = Main.allSubsequences2("abcdefghijklmnop");
+
+        assertEquals(65536, result.size());
+
+        assertTrue(result.contains(""));
+        assertTrue(result.contains("a"));
+        assertTrue(result.contains("p"));
+        assertTrue(result.contains("ap"));
+        assertTrue(result.contains("abcdefghijklmnop"));
+
+        result = Main.allSubsequences2("abc");
+        assertEquals(8, result.size());
+        assertTrue(result.contains(""));
+        assertTrue(result.contains("a"));
+        assertTrue(result.contains("b"));
+        assertTrue(result.contains("c"));
+        assertTrue(result.contains("ab"));
+        assertTrue(result.contains("bc"));
+        assertTrue(result.contains("ac"));
+        assertTrue(result.contains("abc"));*/
     }
 
     @Test
@@ -28,6 +60,10 @@ class MainTest {
         assertEquals(1, Main.minEditDistance("abc", "ab"));
         assertEquals(1, Main.minEditDistance("abc", "abcd"));
         assertEquals(0, Main.minEditDistance("abc", "abc"));
+
+        assertEquals(3, Main.minEditDistance("kitten", "sitting"));
+        assertEquals(3, Main.minEditDistance("Saturday", "Sunday"));
+        assertEquals(1, Main.minEditDistance("twowords", "two words"));
     }
 
     @Test
@@ -35,6 +71,11 @@ class MainTest {
         assertTrue(Main.isSubsequence("abc", "abc"));
         assertFalse(Main.isSubsequence("abc", "a"));
         assertTrue(Main.isSubsequence("abc", "xaxbxxcxxdxxx"));
+
+        assertTrue(Main.isSubsequence("", "anything"));
+        assertFalse(Main.isSubsequence("anything", ""));
+        assertTrue(Main.isSubsequence("", ""));
+        assertFalse(Main.isSubsequence("abc", "ABC"));
     }
 
     @Test
@@ -46,6 +87,12 @@ class MainTest {
         assertEquals("abc", Main.longestCommonSubsequence("abc", "abc"));
         assertEquals("abc", Main.longestCommonSubsequence("abc", "axbxxcxxdxxx"));
         assertEquals("abc", Main.longestCommonSubsequence("yyyayyybyyyyyyc", "axbxxcxxdxxx"));
+
+        assertEquals("", Main.longestCommonSubsequence("abc", ""));
+        assertEquals("", Main.longestCommonSubsequence("", "abc"));
+        assertEquals("", Main.longestCommonSubsequence("", ""));
+        assertEquals("abc", Main.longestCommonSubsequence("abc", "aabbcc"));
+        assertEquals("d", Main.longestCommonSubsequence("abcd", "defg"));
     }
 
     @Test
@@ -56,6 +103,11 @@ class MainTest {
         assertEquals("abc", Main.mergeStrings("b", "ac"));
         assertEquals("abc", Main.mergeStrings("bc", "a"));
         assertEquals("abc", Main.mergeStrings("c", "ab"));
+
+        assertEquals("abcdefg", Main.mergeStrings("abcdef", "g"));
+        assertEquals("abcdefg", Main.mergeStrings("g", "abcdef"));
+        assertEquals("abcdef", Main.mergeStrings("ace", "bdf"));
+
     }
 
     @Test
@@ -65,6 +117,17 @@ class MainTest {
         assertEquals("abc", Main.mergeSort("bca"));
         assertEquals("abc", Main.mergeSort("cab"));
         assertEquals("abc", Main.mergeSort("cba"));
+
+        assertEquals("bbbeglmuu", Main.mergeSort("bubblegum"));
+
+        assertEquals("abcdefghijklmnopqrstuvwxyz", Main.mergeSort("zyxwvutsrqponmlkjihgfedcba"));
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < Integer.MAX_VALUE / 2; i++){
+            sb.append('a');
+        }
+
+        assertNotNull(Main.mergeSort(sb.toString()));
     }
 
     @Test
@@ -77,6 +140,16 @@ class MainTest {
         assertTrue(result.contains("bca"));
         assertTrue(result.contains("cab"));
         assertTrue(result.contains("cba"));
+
+        result = Main.allPermutations("abcd");
+        assertEquals(24, result.size());
+        assertTrue(result.contains("abcd"));
+        assertTrue(result.contains("dcba"));
+        assertTrue(result.contains("adbc"));
+
+        result = Main.allPermutations("");
+        assertEquals(1, result.size());
+        assertTrue(result.contains(""));
     }
 
     @Test
@@ -84,5 +157,11 @@ class MainTest {
         List<String> words = List.of("quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog");
         String result = "The" + Main.lineWrap(words, 15, 15);
         assertEquals("The quick brown\nfox jumps over\nthe lazy dog", result);
+
+        words = List.of( "you", "rather", "have", "unlimited", "bacon", "but", "no", "more", "games",
+                "or", "unlimited", "games", "and", "no", "games");
+        result = "Would" + Main.lineWrap(words, 23, 18);
+        String expected = "Would you rather have\nunlimited bacon but no\nmore games or unlimited\ngames and no games";
+        assertEquals(expected, result);
     }
 }
