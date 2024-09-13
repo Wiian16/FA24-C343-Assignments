cdacfaee38a1f6073cdf8a5dbfffb62af71c4556
Ian McGillivary <wiian16@icloud.com>
2024-09-13 14:55:17 -0400

    Finished PQueue.java and tests

--- A2/test/linear/PQueueTest.java
+++ A2/test/linear/PQueueTest.java
@@ -37,6 +37,37 @@ class PQueueTest {
             }
         }
         assertTrue(queue.isEmpty());
+
+        QueueI<String> queue3 = new PQueue<>();
+
+        assertThrows(EmptyQueueE.class, queue3::dequeue);
+        assertThrows(EmptyQueueE.class, queue3::first);
+        assertTrue(queue3.isEmpty());
+        assertEquals(0, queue3.size());
+
+        for(int i = 1; i <= 5; i++){
+            queue3.enqueue("a".repeat(i));
+        }
+
+        assertFalse(queue3.isEmpty());
+        assertEquals(5, queue3.size());
+
+        try{
+            assertEquals("a", queue3.first());
+
+            while(!queue3.isEmpty()){
+                queue3.dequeue();
+            }
+
+            assertTrue(queue3.isEmpty());
+            assertEquals(0, queue3.size());
+
+            assertThrows(EmptyQueueE.class, queue3::dequeue);
+            assertThrows(EmptyQueueE.class, queue3::first);
+        }
+        catch(EmptyQueueE e){
+            fail();
+        }
     }
 }
 
