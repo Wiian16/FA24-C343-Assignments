dda787c2c63f704227f38243526f397968af7309
Ghafoor, Javaria <jghafoor@iu.edu>
2024-11-01 10:40:02 -0400

    update L9

--- L9/src/TreeConverter.java
+++ L9/src/TreeConverter.java
@@ -11,11 +11,12 @@ public class TreeConverter {
         if (node == null) return; // Base case
 
         //TODO:Fill out the rest of the convertHelper method to complete the conversion from a ternary tree to a trie.
+        // Recursively call convertHelper on the left child without modifying the prefix. (for lexicographically smaller values)
+        // Traverse the main path (middle child), adding current character to the prefix (Track length before adding character)
         // If the current node marks the end of a word, insert the current prefix into the Trie.
-        // Recursively call convertHelper on the left child without modifying the prefix.
         // For the middle child, append the current node’s value to the prefix before the recursive call.
         // After visiting the middle child, backtrack by removing the last character from the prefix to maintain the correct structure.
-        // Finally, call convertHelper on the right child without modifying the prefix.
+        // Finally, call convertHelper on the right child without modifying the prefix. (for lexicographically greater values)
 
     }
 }
