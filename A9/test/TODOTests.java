c08136c20e089da07bc242fbc9f7d3677dab9e72
Ian McGillivary <wiian16@icloud.com>
2024-11-13 23:12:37 -0500

    Removed redundant message

--- A9/test/TODOTests.java
+++ A9/test/TODOTests.java
@@ -32,15 +32,13 @@ public class TODOTests {
         graph.addEdge("C", "D");
 
         Set<String> expectedSet = Set.of("A", "B", "C", "D");
-        assertEquals(expectedSet, graph.getNodes(),
-                String.format("Expected :%s\nActual   :%s", expectedSet, graph.getNodes()));
+        assertEquals(expectedSet, graph.getNodes());
 
         Map<String, Set<Edge>> expectedMap = Map.of("A", Set.of(new Edge("A", "B")),
                 "B", Set.of(new Edge("B", "C")),
                 "C", Set.of(new Edge("C", "D")),
                 "D", Set.of());
-        assertEquals(expectedMap, graph.getAdjacencyLists(),
-                String.format("Expected :%s\nActual   :%s", expectedMap, graph.getNodes()));
+        assertEquals(expectedMap, graph.getAdjacencyLists());
     }
 
     @Test
@@ -57,20 +55,16 @@ public class TODOTests {
 
         expected = Set.of(new Edge("A", "B"), new Edge("A", "C"),
                 new Edge("A", "D"));
-        assertEquals(expected, graph.outgoingEdges("A"),
-                String.format("Expected :%s\nActual   :%s", expected, graph.outgoingEdges("A")));
+        assertEquals(expected, graph.outgoingEdges("A"));
 
         expected = Set.of(new Edge("B", "C"), new Edge ("B", "A"));
-        assertEquals(expected, graph.outgoingEdges("B"),
-                String.format("Expected :%s\nActual   :%s", expected, graph.outgoingEdges("B")));
+        assertEquals(expected, graph.outgoingEdges("B"));
 
         expected = Set.of(new Edge("C", "C"));
-        assertEquals(expected, graph.outgoingEdges("C"),
-                String.format("Expected :%s\nActual   :%s", expected, graph.outgoingEdges("C")));
+        assertEquals(expected, graph.outgoingEdges("C"));
 
         expected = Set.of();
-        assertEquals(expected, graph.outgoingEdges("D"),
-                String.format("Expected :%s\nActual   :%s", expected, graph.outgoingEdges("D")));
+        assertEquals(expected, graph.outgoingEdges("D"));
     }
 
     @Test
@@ -86,20 +80,16 @@ public class TODOTests {
         Set<String> expected;
 
         expected = Set.of("B", "C", "D");
-        assertEquals(expected, graph.neighbors("A"),
-                String.format("Expected :%s\nActual   :%s", expected, graph.neighbors("A")));
+        assertEquals(expected, graph.neighbors("A"));
 
         expected = Set.of("C", "A");
-        assertEquals(expected, graph.neighbors("B"),
-                String.format("Expected :%s\nActual   :%s", expected, graph.neighbors("B")));
+        assertEquals(expected, graph.neighbors("B"));
 
         expected = Set.of("C");
-        assertEquals(expected, graph.neighbors("C"),
-                String.format("Expected :%s\nActual   :%s", expected, graph.neighbors("C")));
+        assertEquals(expected, graph.neighbors("C"));
 
         expected = Set.of();
-        assertEquals(expected, graph.neighbors("D"),
-                String.format("Expected :%s\nActual   :%s", expected, graph.neighbors("C")));
+        assertEquals(expected, graph.neighbors("D"));
     }
 
     @Test
@@ -111,15 +101,13 @@ public class TODOTests {
         DirectedGraph transposed = graph.transpose();
 
         Set<String> expectedNodes = Set.of("A", "B", "C", "D");
-        assertEquals(expectedNodes, transposed.getNodes(),
-                String.format("Expected :%s\nActual   :%s", expectedNodes, transposed.getNodes()));
+        assertEquals(expectedNodes, transposed.getNodes());
 
         Map<String, Set<Edge>> expectedEdges = Map.of("A", Set.of(),
                 "B", Set.of(new Edge("B", "A")),
                 "C", Set.of(new Edge("C", "B")),
                 "D", Set.of(new Edge("D", "C")));
-        assertEquals(expectedEdges, transposed.getAdjacencyLists(),
-                String.format("Expected :%s\nActual   :%s", expectedEdges, transposed.getAdjacencyLists()));
+        assertEquals(expectedEdges, transposed.getAdjacencyLists());
     }
 
     @Test
@@ -131,44 +119,34 @@ public class TODOTests {
         Set<Edge> expected;
 
         expected = Set.of(new Edge("A", "B"), new Edge("A", "E"));
-        assertEquals(expected, adjacencyLists.get("A"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("A")));
+        assertEquals(expected, adjacencyLists.get("A"));
 
         expected = Set.of();
-        assertEquals(expected, adjacencyLists.get("B"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("B")));
+        assertEquals(expected, adjacencyLists.get("B"));
 
         expected = Set.of(new Edge("C", "J"), new Edge("C", "A"));
-        assertEquals(expected, adjacencyLists.get("C"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("C")));
+        assertEquals(expected, adjacencyLists.get("C"));
 
         expected = Set.of(new Edge("D", "C"));
-        assertEquals(expected, adjacencyLists.get("D"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("D")));
+        assertEquals(expected, adjacencyLists.get("D"));
 
         expected = Set.of();
-        assertEquals(expected, adjacencyLists.get("E"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("E")));
+        assertEquals(expected, adjacencyLists.get("E"));
 
         expected = Set.of(new Edge("F", "H"), new Edge("F", "D"));
-        assertEquals(expected, adjacencyLists.get("F"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("F")));
+        assertEquals(expected, adjacencyLists.get("F"));
 
         expected = Set.of(new Edge("G", "H"), new Edge("G", "B"));
-        assertEquals(expected, adjacencyLists.get("G"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("G")));
+        assertEquals(expected, adjacencyLists.get("G"));
 
         expected = Set.of(new Edge("H", "I"), new Edge("H", "A"));
-        assertEquals(expected, adjacencyLists.get("H"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("H")));
+        assertEquals(expected, adjacencyLists.get("H"));
 
         expected = Set.of(new Edge("I", "E"), new Edge("I", "G"));
-        assertEquals(expected, adjacencyLists.get("I"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("I")));
+        assertEquals(expected, adjacencyLists.get("I"));
 
         expected = Set.of(new Edge("J", "F"));
-        assertEquals(expected, adjacencyLists.get("J"),
-                String.format("Expected :%s\nActual   :%s", expected, adjacencyLists.get("J")));
+        assertEquals(expected, adjacencyLists.get("J"));
     }
 
     // DFS Tests
@@ -184,8 +162,7 @@ public class TODOTests {
         List<String> traversal = dfs.getTraversal();
 
         List<String> expected = List.of("A", "B", "C", "D");
-        assertEquals(expected, traversal,
-                String.format("Expected :%s\nActual   :%s", expected, traversal));
+        assertEquals(expected, traversal);
     }
 
     @Test
@@ -199,12 +176,10 @@ public class TODOTests {
         List<String> expected;
 
         expected = List.of("A", "B");
-        assertEquals(expected, dfs.getAllTraversals().get("A"),
-                String.format("Expected :%s\nActual   :%s", expected, dfs.getAllTraversals().get("A")));
+        assertEquals(expected, dfs.getAllTraversals().get("A"));
 
         expected = List.of("C", "D");
-        assertEquals(expected, dfs.getAllTraversals().get("C"),
-                String.format("Expected :%s\nActual   :%s", expected, dfs.getAllTraversals().get("C")));
+        assertEquals(expected, dfs.getAllTraversals().get("C"));
     }
 
     @Test
@@ -214,8 +189,7 @@ public class TODOTests {
         dfs.traverse("A");
 
         List<String> expected = List.of("A", "B", "C", "D");
-        assertEquals(expected, dfs.getTraversal(),
-                String.format("Expected :%s\nActual   :%s", expected, dfs.getTraversal())); //todo: remove all messages
+        assertEquals(expected, dfs.getTraversal()); //todo: remove all messages
     }
 
     @Test
